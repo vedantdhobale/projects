3250c7d199df91d089529dccd7564dc236c36250
vedantdhobale <vedantdhobale1998@gmail.com>
2022-12-11 12:53:24 +0530

    changes modified

--- Demo.java
+++ Demo.java
@@ -3,6 +3,8 @@ public class main{
 
 public static void main(String[] args){
 
-System.out.println("This is forst for git test");
+System.out.println("This is first for git test");
+
+Sysout("this is the second test for git");
 }
 }
